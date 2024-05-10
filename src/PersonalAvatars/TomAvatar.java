3fd3e009bf989d154e160ba85bd9e58ce488a263
TomKreye <tom.kreye1@gmail.com>
2024-05-10 11:11:01 +0200

    Zweier Entwurf Avatar
    Tom

--- src/PersonalAvatars/TomAvatar.java
+++ src/PersonalAvatars/TomAvatar.java
@@ -3,37 +3,36 @@ import Environment.*;
 
 import java.util.ArrayList;
 
+import javax.swing.DropMode;
+
 import AvatarInterface.*;
 
 
 public class TomAvatar extends SuperAvatar {
 
-    private int counter;
 
     public TomAvatar(int id) {
         super(id);
-        this.counter = 0;
-        // TODO Auto-generated constructor stub
     }
 
     @Override
     public Direction yourTurn(ArrayList<SpaceInfo> spacesInRange) {
 
-        switch (counter) {
-            case 0: return Direction.RIGHT; 
-            case 1: return Direction.DOWN; 
-            case 2: return Direction.LEFT;
-            case 3: return Direction.UP;  
-        
+   
+        for(SpaceInfo infos : spacesInRange){
+            if(infos.getType() == SpaceType.AVATAR){
+                Coordinate coordinateOfAvatar = infos.getRelativeToAvatarCoordinate();
+                if(coordinateOfAvatar.getY() < 0)
+                    return Direction.UP;
+                if(coordinateOfAvatar.getY() > 0)
+                    return Direction.DOWN;
+                if(coordinateOfAvatar.getX() < 0)
+                    return Direction.LEFT;
+                if(coordinateOfAvatar.getX() > 0)
+                    return Direction.RIGHT;
+            }
+            return Direction.STAY;
         }
-        counter++;
-
-        if (counter >= 3) {
-            counter = 0;
-        }
-
-        return null;
-
+        return Direction.STAY;
     }
-
 }
