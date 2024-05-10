4555acd898ad07c028a783d3985eba2918c30fbe
PhilippRiefer <66263398+PhilippRiefer@users.noreply.github.com>
2024-05-10 09:07:52 +0200

    Update IvenAvatar.java

--- src/PersonalAvatars/IvenAvatar.java
+++ src/PersonalAvatars/IvenAvatar.java
@@ -1,15 +1,15 @@
 package PersonalAvatars;
 
 import AvatarInterface.*;
-import java.util.ArrayList;
 import Environment.*;
+import java.util.ArrayList;
 
 public class IvenAvatar extends SuperAvatar { // implements AvatarInterface
     
     int needToDance = 100;
-    int needToDrink = 100;
     int needToPee = 100;
     int needToRest = 100;
+	int neetToTalk = 100;
 
 	public IvenAvatar(int id) {
 		super(id);
@@ -17,24 +17,33 @@ public class IvenAvatar extends SuperAvatar { // implements AvatarInterface
 
 	@Override
 	public Direction yourTurn(ArrayList<SpaceInfo> spacesInRange) {
-        for (int i = 0; i>4;i++){
-            SpaceType frontTyp = spacesInRange.getType;
-            Coordinate frontCoord = spacesInRange.get(i).getRelativeToAvatarCoordinate;
-        }
         
+		Coordinate dir_  = new Coordinate(1,0);
+		for (int i=0;i<4;i++){
+	
+			if(spacesInRange.get(i).getRelativeToAvatarCoordinate().equals(test)){}
+        SpaceType frontTyp = spacesInRange.get(1).getType();
+        Coordinate frontCoord = spacesInRange.get(1).getRelativeToAvatarCoordinate();
+        SpaceType rightTyp = spacesInRange.get(1).getType();
+        Coordinate rightCoord = spacesInRange.get(1).getRelativeToAvatarCoordinate();
+        SpaceType backTyp = spacesInRange.get(1).getType();
+        Coordinate backCoord = spacesInRange.get(1).getRelativeToAvatarCoordinate();
+        SpaceType leftTyp = spacesInRange.get(1).getType();
+        Coordinate leftCoord = spacesInRange.get(1).getRelativeToAvatarCoordinate();        
+	}
 		int max = 3;
 		int min = 0;
 		int directionNumber = (int) (Math.random() * ((max - min) + 1) + min);
 
-		switch (directionNumber) {
-			case 0: return Direction.LEFT;
-			case 1: return Direction.RIGHT;
-			case 2: return Direction.UP;
-			case 3: return Direction.DOWN;
-
-			default: return Direction.STAY;
-		}	
+        return switch (directionNumber) {
+            case 0 -> Direction.LEFT;
+            case 1 -> Direction.RIGHT;
+            case 2 -> Direction.UP;
+            case 3 -> Direction.DOWN;
+            default -> Direction.STAY;
+        };	
 	}
+
 
 	
 }
