8b78baf098440110d0f24b6f8b70f9976cc44d96
TomKreye <tom.kreye1@gmail.com>
2024-05-17 11:11:51 +0200

    Update RobinAvatarComplex.java
    Add updateEnvironment method:
    Fills the personal Environment with the recived information

--- src/PersonalAvatars/RobinAvatarComplex.java
+++ src/PersonalAvatars/RobinAvatarComplex.java
@@ -18,17 +18,29 @@ public class RobinAvatarComplex extends SuperAvatar {
     }
 
     enum PersonalFieldType{
-        EMPTY(true),
-        WALKED(true),
-        WALL(false),
-        UNKNOWN(false),
-        REACHABLE(false);
+        EMPTY(true,false),
+        WALKED(true,false),
+        WALL(false,false),
+        UNKNOWN(false,true),
+        REACHABLE(false,true);
 
         private final boolean walkable;
+        private final boolean unknown;
 
-        PersonalFieldType(boolean walkable){
+        PersonalFieldType(boolean walkable, boolean unknown){
             this.walkable = walkable;
+            this.unknown = unknown;
         }
+
+        public boolean isWalkable() {
+            return walkable;
+        }
+
+        public boolean isUnknown() {
+            return unknown;
+        }
+
+        
     }
 
 
@@ -55,10 +67,7 @@ public class RobinAvatarComplex extends SuperAvatar {
     public Direction yourTurn(ArrayList<SpaceInfo> spacesInRange) {
         // TODO Auto-generated method stub
         //throw new UnsupportedOperationException("Unimplemented method 'yourTurn'");
-        for (SpaceInfo space : spacesInRange) {
-            if(space.getType() == SpaceType.EMPTY)
-                return coordinateToDirection(space.getRelativeToAvatarCoordinate());
-        }
+        updateEnvironment(spacesInRange);
         
         
         return Direction.STAY;
@@ -77,7 +86,19 @@ public class RobinAvatarComplex extends SuperAvatar {
     }
 
     private void updateEnvironment(ArrayList<SpaceInfo> spacesInRange){
-
+        for (SpaceInfo spaceInfo : spacesInRange) {
+            Coordinate spaceRelPos = spaceInfo.getRelativeToAvatarCoordinate();
+            Coordinate spaceAbsPos = new Coordinate(position.getX() + spaceRelPos.getX(), position.getY() + spaceRelPos.getY());
+            if(((PersonalFieldType) (environment[spaceAbsPos.getX()][spaceAbsPos.getY()][1])).isUnknown()){
+                environment[spaceAbsPos.getX()][spaceAbsPos.getY()][0] = spaceInfo.getType();
+                if(spaceInfo.getType() == SpaceType.EMPTY || spaceInfo.getType() == SpaceType.AVATAR){
+                    environment[spaceAbsPos.getX()][spaceAbsPos.getY()][1] = PersonalFieldType.EMPTY;
+                }
+                else{
+                    environment[spaceAbsPos.getX()][spaceAbsPos.getY()][1] = PersonalFieldType.WALL;
+                }
+            }
+        }
     }
 
 }
