7ac7c13929da427fe89f0fad0f17541364fcc85a
m4x1m-br3st3l <166661594+m4x1m-br3st3l@users.noreply.github.com>
2024-05-17 08:40:30 +0200

    added basic functions
    added yourTurn(), randomDirection(), getPerceptionRange() and setPerceptionRange()

--- src/PersonalAvatars/MaximAvatar_Spock.java
+++ src/PersonalAvatars/MaximAvatar_Spock.java
@@ -8,16 +8,65 @@ import Environment.SpaceInfo;
 
 public class MaximAvatar_Spock extends SuperAvatar {
 
-    public MaximAvatar_Spock(int id) {
-        super(id);
+    public MaximAvatar_Spock(int id, int perceptionRange) {
+        super(id, perceptionRange);
         //TODO Auto-generated constructor stub
     }
 
+    /**
+     * Determines the direction for the avatar's next turn based on the spaces within its perception range.
+     * This method can be overridden to implement a more sophisticated strategy.
+     *
+     * @param spacesInRange the list of spaces within the avatar's perception range
+     * @return the direction for the avatar's next turn
+     */
     @Override
     public Direction yourTurn(ArrayList<SpaceInfo> spacesInRange) {
-        // TODO Auto-generated method stub
-        //throw new UnsupportedOperationException("Unimplemented method 'yourTurn'");
-        return Direction.STAY;
+        // Implement a more sophisticated strategy using spacesInRange
+        // For now, let's continue to move randomly as a placeholder
+        return randomDirection();
+    }
+
+    /**
+     * Generates a random direction for the avatar to move.
+     *
+     * @return a random direction
+     */
+    private Direction randomDirection() {
+        int directionNumber = (int) (Math.random() * 4);
+
+        switch (directionNumber) {
+            case 0:
+                return Direction.LEFT;
+            case 1:
+                return Direction.RIGHT;
+            case 2:
+                return Direction.UP;
+            case 3:
+                return Direction.DOWN;
+            default:
+                return Direction.STAY; // Safety net, though unnecessary as directionNumber is bound by 0-3
+        }
+    }
+
+    /**
+     * Returns the perception range of the avatar.
+     *
+     * @return the perception range
+     */
+    @Override
+    public int getPerceptionRange() {
+        return super.getPerceptionRange(); // Assuming SuperAvatar has a method to get the perception range
+    }
+
+    /**
+     * Sets the perception range of the avatar.
+     *
+     * @param perceptionRange the perception range to set
+     */
+    @Override
+    public void setPerceptionRange(int perceptionRange) {
+        super.setPerceptionRange(perceptionRange); // Set the perception range via the superclass method
     }
     
 }
