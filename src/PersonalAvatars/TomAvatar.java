54edc10176f9c616e20e5e7adf4780341ac2ff70
TomKreye <tom.kreye1@gmail.com>
2024-05-31 11:31:11 +0200

    Programming Avatar goes to starting Point for moving spiral

--- src/PersonalAvatars/TomAvatar.java
+++ src/PersonalAvatars/TomAvatar.java
@@ -3,93 +3,140 @@ package PersonalAvatars;
 import Environment.*;
 import java.util.ArrayList;
 
+import org.reflections.vfs.Vfs.Dir;
 
 import AvatarInterface.*;
 import java.awt.Color;
 
-
- //find my position and set to (0,0)
-        //spacesInRange
-        /* 0 |  3  | 5
-         * 1 |  ME | 6
-         * 2 |  4  | 7
-        */
-
+//find my position and set to (0,0)
+//spacesInRange
+/* 0 |  3  | 5
+ * 1 |  ME | 6
+ * 2 |  4  | 7
+*/
 
 public class TomAvatar extends SuperAvatar {
 
     private static final int mapSizeX = 40;
     private static final int mapSizeY = 20;
     private SpaceType[][] mentalMap;
-    private int storePerceptionRangeX, storePerceptionRangeY = 0;
-
+    private int storePerceptionRange = 0;
+    private int phase = 0, stepCounter = 0, directionCounter = 0;
+    private Coordinate myCoordinate;
 
     public TomAvatar(int id, int perceptionRange, Color color) {
         super(id, perceptionRange, color);
-        storePerceptionRangeX = perceptionRange;
-        storePerceptionRangeY = perceptionRange;
+        storePerceptionRange = perceptionRange;
         this.mentalMap = new SpaceType[mapSizeX][mapSizeY];
+        this.myCoordinate = new Coordinate(0, 0);
 
     }
 
-    public void createMentalMap(ArrayList<SpaceInfo> spacesInRange){
-        for(int i = 0; i < spacesInRange.size(); i++){
-            if(i < 3){
-                mentalMap[spacesInRange.get(0).getRelativeToAvatarCoordinate().getX()]
-                [spacesInRange.get(i).getRelativeToAvatarCoordinate().getY()] = spacesInRange.get(i).getType();
-            }else if(i < 5){
-                mentalMap[spacesInRange.get(3).getRelativeToAvatarCoordinate().getX()]
-                [spacesInRange.get(i).getRelativeToAvatarCoordinate().getY()] = spacesInRange.get(i).getType();
-            }else{
-                mentalMap[spacesInRange.get(5).getRelativeToAvatarCoordinate().getX()]
-                [spacesInRange.get(i).getRelativeToAvatarCoordinate().getY()] = spacesInRange.get(i).getType();
+    public void createMentalMap(ArrayList<SpaceInfo> spacesInRange) {
+        for (int i = 0; i < spacesInRange.size(); i++) {
+            if (i < 3) {
+                mentalMap[spacesInRange.get(0).getRelativeToAvatarCoordinate().getX()][spacesInRange.get(i)
+                        .getRelativeToAvatarCoordinate().getY()] = spacesInRange.get(i).getType();
+            } else if (i < 5) {
+                mentalMap[spacesInRange.get(3).getRelativeToAvatarCoordinate().getX()][spacesInRange.get(i)
+                        .getRelativeToAvatarCoordinate().getY()] = spacesInRange.get(i).getType();
+            } else {
+                mentalMap[spacesInRange.get(5).getRelativeToAvatarCoordinate().getX()][spacesInRange.get(i)
+                        .getRelativeToAvatarCoordinate().getY()] = spacesInRange.get(i).getType();
             }
-        } 
+        }
     }
 
-    public void getMyPosition(ArrayList<SpaceInfo> spacesInRange){
-        Coordinate markerPosition = spacesInRange.get(0).getRelativeToAvatarCoordinate();
-        SpaceType markerPositionType = spacesInRange.get(0).getType();
-        System.out.println(storePerceptionRangeX);
+    public void getMyPosition(ArrayList<SpaceInfo> spacesInRange) {
 
-        if(markerPositionType == SpaceType.OBSTACLE & storePerceptionRangeX > 1 )
-            storePerceptionRangeX--;
-        else if(markerPositionType == SpaceType.OBSTACLE & markerPosition.getY() > storePerceptionRangeY)
-            storePerceptionRangeY--;
+        for (int i = 0; i < spacesInRange.size(); i++) {
+            if ((spacesInRange.get(i).getRelativeToAvatarCoordinate().getX() == spacesInRange.get(i + 1)
+                    .getRelativeToAvatarCoordinate().getX()) &
+                    (spacesInRange.get(i).getRelativeToAvatarCoordinate().getY() -
+                            spacesInRange.get(i + 1).getRelativeToAvatarCoordinate().getY()) == -2) {
 
-
-        Coordinate myPosition = new Coordinate(markerPosition.getX() + storePerceptionRangeX, markerPosition.getY() + storePerceptionRangeY);
-        System.out.println("X" + myPosition.getX() + "Y" + myPosition.getY());
+                myCoordinate.setX(spacesInRange.get(i).getRelativeToAvatarCoordinate().getX());
+                myCoordinate.setY(spacesInRange.get(i).getRelativeToAvatarCoordinate().getY() + 1);
+                System.out.println("X" + myCoordinate.getX() + "Y" + myCoordinate.getY());
+                break;
+            }
+        }
     }
 
-    public Direction goToEmpty(ArrayList<SpaceInfo> spacesInRange){
-        /* 
-        for(int i = 0; i < spacesInRange.size(); i++){
-            if(spacesInRange.get(i).getType() == SpaceType.EMPTY){        
-                if(i == 1){
-                        return Direction.LEFT;
-                    }else if(i == 3){
-                        return Direction.UP;
-                    }else if(i == 4){
-                        return Direction.DOWN;             
-                    }else if(i == 6){
-                        return Direction.RIGHT;
-                        }
-                    }
-                }
-        return Direction.DOWN;
-    }*/
-    return Direction.LEFT;
-}
+    public Direction goToStart() {
+
+        if (myCoordinate.getX() > 19)
+            return Direction.LEFT;
+        else if (myCoordinate.getX() < 19)
+            return Direction.RIGHT;
+        else if (myCoordinate.getY() > 9)
+            return Direction.UP;
+        else if (myCoordinate.getY() < 9)
+            return Direction.DOWN;
+        else
+            return Direction.STAY;
+
+    }
 
     
+    public Direction startSpiral(ArrayList<SpaceInfo> spacesInRange){
+
+        while(myCoordinate.getX() > 1){
+            if(directionCounter == 0 & stepCounter <= 3 + storePerceptionRange){
+                if(stepCounter == 3 + storePerceptionRange){
+                    stepCounter = 0;
+                    directionCounter = 1;
+                }        
+                return Direction.RIGHT;
+            } else if(directionCounter == 1 & stepCounter <= 3 + storePerceptionRange){
+                if(stepCounter == 3 + storePerceptionRange){
+                    stepCounter = 0;
+                    directionCounter = 2;
+                }
+                return Direction.UP;
+            } else if(directionCounter == 2 & stepCounter <= (3 + storePerceptionRange) * 2){
+                if(stepCounter == (3 + storePerceptionRange) * 2 ){
+                    stepCounter = 0;
+                    directionCounter = 3;
+                }
+                return Direction.LEFT;
+            }else if(directionCounter == 2 & stepCounter <= (3 + storePerceptionRange) * 2){
+                if(stepCounter == (3 + storePerceptionRange) * 2){
+                    stepCounter = 0;
+                    directionCounter = 2;
+                }
+                return Direction.DOWN;
+            }
+        }
+        return Direction.STAY;
+    }
+
     @Override
     public Direction yourTurn(ArrayList<SpaceInfo> spacesInRange) {
-        getMyPosition(spacesInRange);
-        createMentalMap(spacesInRange);
-        return goToEmpty(spacesInRange);       
         
-        
+        // createMentalMap(spacesInRange);
+        Direction dir = Direction.STAY;
+        int phase = 0;
+
+        if(myCoordinate.getX() == 19 & myCoordinate.getY() == 9){
+            phase = 1;
+        }
+
+
+        switch (phase) {
+            case 0:
+            getMyPosition(spacesInRange);
+            dir = goToStart();
+            break;
+            case 1: 
+            dir = startSpiral(spacesInRange);
+            stepCounter++;
+            break;
+            default:
+                break;
+        }
+        return dir;
+
     }
 
     @Override
--- src/SimulationControl/Main.java
+++ src/SimulationControl/Main.java
@@ -14,7 +14,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // Scanner scanner = new Scanner(System.in);
-        int perceptionRange = 2;
+        int perceptionRange = 1;
         System.out.println("Perception range is set to " + perceptionRange);
         // boolean validInput = false;
 
