5d93d06c7e164a5496f64ba18e01adc2d7c5828b
Soodeh84 <67098525+Soodeh84@users.noreply.github.com>
2024-06-07 09:06:50 +0200

    Update paolaAvatar

--- src/PersonalAvatars/PaolaAvatar.java
+++ src/PersonalAvatars/PaolaAvatar.java
@@ -4,12 +4,72 @@ import AvatarInterface.SuperAvatar;
 import Environment.*;
 import java.awt.Color;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
- * This class represents a template avatar that extends the SuperAvatar class and implements the AvatarInterface.
- * It provides a basic implementation of the avatar's behavior and perception range.
+ * Paola's avatar
  */
 public class PaolaAvatar extends SuperAvatar { // implements AvatarInterface
+
+    final private int ENERGY_MAX = 2400; // enough to visit the 800 cells in the room 3 times, as each turn deducts one energy point
+    final private int ENERGY_LOW_WARNING = 50; // for interrupt to go visit the bar for a drink
+    final private int ENERGY_MIN = 0; // if 0, fall asleep and replenish
+
+    final private int FULL_BLADDER = 1000; // TODO: Define behavior for when full bladder is reached
+    final private int BLADDER_HIGH_WARNING = 900; // to prioritize visiting the toilet
+    final private int EMPTY_BLADDER = 0; 
+
+    final private int SOCIAL_MAX = 1000; // wants to find other avatars
+    final private int SOCIAL_MIN = 0; // does not care for other avatars
+
+    final private int FUN_MAX = 1000; // does not care about going to dancefloor
+    final private int FUN_MIN = 0; // wants to have fun in the dancefloor or djbooth
+    
+
+    SpaceType[][] internalMap = new SpaceType[40][20];
+    private boolean internalMapCompleteFlag = false; // flag changes to "true" when map has been complete
+                                                    // TODO: Design algorithm to detect that map has been complete 
+                                                    // and change this flag.
+    private boolean hasGoalInMind = false; // flag changes to "true" when a purpose has been selected (e.g. go to bar)
+                                                    // TODO: Flag must change back to "false" when purpose has been completed   
+    private int energy; // when low on energy, Paola will want to go to the bar and get a drink
+    private int bladder; // when high, Paola will seek the bathroom
+    private int social; // when social is high, Paola will be more likely to interact with other avatars
+    private int fun; // when fun is low, Paola will want be more likely to go dancing
+
+    private Goal goal;
+    
+    private class Goal {
+        private SpaceType target;
+        private Coordinate[][][] route; // Array of steps in route. Each step in the route has an array of 3 Coordinates
+        private boolean fulfilled;
+
+        public void setTarget(SpaceType target) {
+            this.target = target;
+        }
+
+        public void setFulfilled(boolean fulfilled) {
+            this.fulfilled = fulfilled;
+        }
+
+        public SpaceType getTarget() {
+            return target;
+        }
+
+        public boolean isFulfilled() {
+            return fulfilled;
+        }
+
+        void calculateRoute(){
+            // TODO
+        }
+
+        Coordinate nextMilestone(){
+            // TODO
+            return null;
+        }
+        
+    }
 
     /**
      * Constructs a TemplateAvatar object with the specified ID and perception range.
@@ -18,7 +78,13 @@ public class PaolaAvatar extends SuperAvatar { // implements AvatarInterface
      * @param perceptionRange the perception range of the avatar
      */
     public PaolaAvatar(int id, int perceptionRange, Color color) {
-        super(id, perceptionRange, color);
+        super(id, perceptionRange, Color.decode("#8b1a93"));
+        Random random = new Random();
+        energy = random.nextInt(ENERGY_LOW_WARNING,ENERGY_MAX); // initialize with half of the energy to full energy
+        bladder = 0; // empty bladder
+        social = random.nextInt(SOCIAL_MIN, SOCIAL_MAX); // initialize with half of the desire to be social to full desire
+        fun = random.nextInt(FUN_MIN, FUN_MAX); // low on fun means wants to dance
+        this.goal = new Goal();
     }
 
     /**
@@ -30,9 +96,58 @@ public class PaolaAvatar extends SuperAvatar { // implements AvatarInterface
      */
     @Override
     public Direction yourTurn(ArrayList<SpaceInfo> spacesInRange) {
-        // Implement a more sophisticated strategy using spacesInRange
+        // update internal map
+        if (!internalMapCompleteFlag){
+            updateInternalMap(spacesInRange);
+        }
+        
+
+        if(hasGoalInMind){
+            if (energy > ENERGY_LOW_WARNING){ // has enough energy
+                if (bladder < BLADDER_HIGH_WARNING){ // does not need to go to toilet
+                    Direction nextMove = calculateDirection(goal.nextMilestone());
+                    return nextMove;
+                } else {
+
+                }
+
+            }
+        }
+        
+        
+        
+        // select purpose based on current need
+        // set a purpose flag, so that it does not interfere with other needs and needs aren't 
+            // changing with every call to yourTurn
+        // if purpose flag is raised:
+            // retrieve location of desired target
+            // calculate direction to go to target
+            // return that direction
+        // if purpose flag is not raised, choose a purpose based on needs
+
+        
+        
+        
+        
         // For now, let's continue to move randomly as a placeholder
         return randomDirection();
+    }
+
+    private Direction calculateDirection(Coordinate nextMilestone) {
+        // Calculate direction based on current position and next milestone
+        
+        return Direction.DOWN;
+    }
+
+    private void updateInternalMap(ArrayList<SpaceInfo> spacesInRange) {
+        for(SpaceInfo space:spacesInRange){
+            SpaceType spaceType = space.getType();
+            if(spaceType != SpaceType.AVATAR){
+                Coordinate spaceCoordinate = space.getRelativeToAvatarCoordinate();
+                internalMap[spaceCoordinate.getX()][spaceCoordinate.getY()] = spaceType;
+            }
+            
+        }
     }
 
     /**
