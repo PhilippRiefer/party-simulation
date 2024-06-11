c932215e3eefe94b0653e17af8d199a1e95ea6c5
RobinErasmus <166122169+RobinErasmus@users.noreply.github.com>
2024-06-11 12:24:56 +0200

    Update SimulationControl.java
    change step time

--- src/SimulationControl/SimulationControl.java
+++ src/SimulationControl/SimulationControl.java
@@ -78,7 +78,7 @@ public class SimulationControl {
                 boolean hasMoved = environment.moveAvatar(avatar.getAvatarID(), dir, avatar.getAvatarColor());
                 avatar.setHasMoved(hasMoved);
                 System.out.println("Avatar" + avatar.getAvatarID() + " has moved = " + hasMoved);
-                wait(20);
+                wait(40);
             } catch (Exception e) {
                 System.err.println("Error processing avatar " + avatar.getAvatarID() + ": " + e.getMessage());
                 e.printStackTrace();
