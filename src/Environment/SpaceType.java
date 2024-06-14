db30a94d785151332a7648ac365ceec2d0f11d1a
Soodeh84 <67098525+Soodeh84@users.noreply.github.com>
2024-06-14 10:24:19 +0200

    clean up Environment package!

--- src/Environment/SpaceType.java
+++ src/Environment/SpaceType.java
@@ -1,9 +1,9 @@
+package Environment;
+
 /**
  * The SpaceType enum represents different types of spaces in a party simulation environment.
  * The types are EMPTY, OBSTACLE, AVATAR, DANCEFLOOR, DJBOOTH, TOILET, BAR, and SEATS.
  */
-package Environment;
-
 
 public enum SpaceType {
     EMPTY,
