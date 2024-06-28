abfc5e66e73c72d89fcdec663391c97afb20dae6
PhilippRiefer <66263398+PhilippRiefer@users.noreply.github.com>
2024-06-28 10:10:40 +0200

    end commit

---
+++ src/Environment/MinimapWriter.java
@@ -0,0 +1,16 @@
+package Environment;
+
+import java.util.HashMap;
+
+public class MinimapWriter {
+
+	public HashMap addTile(HashMap minimap, Coordinate coordinate, SpaceType spaceType) {
+		// if the coordinate is not in the map, add it
+		// if the coordinate is on the map, but has a different space type, update it
+		// so basically always overwrite it
+
+		
+
+		return minimap;
+	}
+}
---
+++ src/Environment/Tile.java
@@ -0,0 +1,33 @@
+package Environment;
+
+import java.awt.Color;
+
+/**
+ * The Tile class represents a single tile on the minimap.
+ */
+public class Tile {
+    int x, y;
+    Color color;
+
+    /**
+     * Constructs a Tile with the specified coordinates and color.
+     *
+     * @param x     The x-coordinate of the tile.
+     * @param y     The y-coordinate of the tile.
+     * @param color The color of the tile.
+     */
+    public Tile(int x, int y, Color color) {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Tile{" +
+                "x=" + x +
+                ", y=" + y +
+                ", color=" + color +
+                '}';
+    }
+}
